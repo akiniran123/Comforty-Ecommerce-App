15dc96cadbba83c9d43d7608c4a2f1073b339e13
nguyentuannghia98 <meobietbay09@gmail.com>
2025-07-27 14:36:14 +0700

    d

--- src/Components/Navbar/NavbarMiddle.jsx
+++ src/Components/Navbar/NavbarMiddle.jsx
@@ -5,18 +5,20 @@ const NavbarMiddle = () => {
   return (
     <div className="navbar_middle flex items-center justify-center bg-[#f0f2f3] w-full h-[84px]">
       <div className="lg:container grid grid-cols-3 items-center">
+        {/* Logo */}
         <div className="logo_wrapper">
           <Link to="/" className="flex items-center">
             <img src="/src/assets/logo.png" alt="Logo" className="w-28 h-28" />
           </Link>
         </div>
 
+        {/* Search box */}
         <div className="search_box">
           <form action="#" className="max-w-[443px] h-[44px] relative">
             <input
               type="text"
               placeholder="Search here..."
-              className="max-w-[443px] w-full h-full bg-white rounded-lg pl-4"
+              className="max-w-[443px] w-full h-full bg-white text-black rounded-lg pl-4 placeholder:text-gray-500"
             />
             <button type="button" className="absolute top-1/2 right-4 -translate-y-1/2">
               <Search size="22px" color="#5a5f99" />
@@ -24,7 +26,8 @@ const NavbarMiddle = () => {
           </form>
         </div>
 
-        <div className="navbar_middle_right flex items-center gap-4">
+        {/* Icons */}
+        <div className="navbar_middle_right flex items-center gap-4 justify-end">
           <button className="btn capitalize">
             <ShoppingCart /> Giỏ hàng
             <div className="badge badge-sm bg-[#029fae]">0</div>
@@ -37,7 +40,7 @@ const NavbarMiddle = () => {
             <div tabIndex={0} role="button" className="btn m-1">
               <User />
             </div>
-            <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+            <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-10 w-52 p-2 shadow-sm">
               <li><Link to="/account">Tài Khoản</Link></li>
               <li><Link to="/logout">Đăng Xuất</Link></li>
             </ul>
@@ -49,4 +52,3 @@ const NavbarMiddle = () => {
 };
 
 export default NavbarMiddle;
-
